6a73a98a88a4d403fc0fd2760cb6feafe99b7201
Angelo Zero <angelo.akm@gmail.com>
2025-05-31 22:14:57 -0300

    todo

--- src/main/java/com/angelozero/task/management/adapter/dataprovider/EventConsumerByRabbitMQDataProvider.java
+++ src/main/java/com/angelozero/task/management/adapter/dataprovider/EventConsumerByRabbitMQDataProvider.java
@@ -30,7 +30,7 @@ public class EventConsumerByRabbitMQDataProvider {
             System.out.println("[Consumer] Received EventEntity: " + receivedEvent);
             System.out.println("[Consumer] Processing EventType: " + receivedEvent.eventType());
             System.out.println("[Consumer] Processing Message: " + receivedEvent.message());
-            // TODO Sua lógica de processamento do evento aqui ... menos hoje
+            // TODO Sua lógica de processamento do evento aqui ... menos hoje, e hoje tambem nao
 
         } catch (Exception ex) {
             System.err.println("[Consumer] Error deserializing message: " + ex.getMessage());
